3527878ebf7bb14118af40421f395aefbba77fe8
Kyrylo Kusovskyi <kkirilll2006@gmail.com>
2026-05-10 13:20:20 +0300

    Initial commit

---
+++ src/main/kotlin/labs/currencyservice/CurrencyServiceApplication.kt
@@ -0,0 +1,11 @@
+package labs.currencyservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CurrencyServiceApplication
+
+fun main(args: Array<String>) {
+    runApplication<CurrencyServiceApplication>(*args)
+}
